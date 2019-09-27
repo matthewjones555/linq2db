06c34d1d28d83568a96dfa49f4034e3ebd8d07dc
Matthew Jones <matthew.jones@slimmingworld.co.uk>
2019-09-27 11:40:06 +0100

    Add build configuration to local pack and publish script

--- Nuget pack and publish Linq2DB.ps1
+++ Nuget pack and publish Linq2DB.ps1
@@ -15,12 +15,13 @@ echo $version
 
 $currentDirectory = [System.IO.DirectoryInfo]::new([System.IO.Directory]::GetCurrentDirectory())
 
-$slnFileName = "linq2db.sln"
+$slnFileName = "linq2db.ci.build.sln"
+$configuration = "Debug"
 
 # build the solution
 [System.Console]::WriteLine("Building " + $slnFileName)
-cmd.exe /c echo "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" $slnFileName /p:Version=$version -target:Rebuild -v:quiet
-& "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" $slnFileName /p:Version=$version -target:Rebuild -v:quiet
+cmd.exe /c echo "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" $slnFileName /p:Version=$version -target:Rebuild /p:Configuration=$configuration -v:quiet
+& "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" $slnFileName /p:Version=$version -target:Rebuild /p:Configuration=$configuration -v:quiet
 
 $outputDirectory = [System.IO.DirectoryInfo]::new("BuiltPackages")
 
@@ -28,13 +29,14 @@ if ($outputDirectory.Exists) {
     $outputDirectory.Delete($true)
 }
 
+# if the status of directory has changed since we created the DirectoryInfo, the information will be wrong, hence we need a new instance
 $outputDirectory = [System.IO.DirectoryInfo]::new("BuiltPackages")
 
 if (-Not $outputDirectory.Exists) {
     $outputDirectory.Create()
 }
 
-nuget.exe pack Nuget\linq2db.nuspec -OutputDirectory $outputDirectory.FullName -Version $version -Properties id=linq2db.legacy.sqlce
+nuget.exe pack Nuget\linq2db.nuspec -OutputDirectory $outputDirectory.FullName -Version $version -Properties id=linq2db.legacy.sqlce -Properties Configuration=$configuration
 
 $files = $outputDirectory.GetFiles("*.nupkg")
 
